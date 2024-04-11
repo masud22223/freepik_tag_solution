ae16e002ec71f59fdeaa1b7cbcfdd9eb027a06c3
masud22223 <137398264+masud22223@users.noreply.github.com>
2024-04-11 10:57:48 -0500

    Update solution.php

--- solution.php
+++ solution.php
@@ -4,7 +4,101 @@
 
 <?php 
 
-$str = "Banner banner Pattern Poster Label Cover Texture Paper Beauty Typography Layout Packaging Luxury Art Text Advertising Sign Silver Makeup Elegant Golden Branding Product Clean Symbol Skin Brand Care Healthcare Cream Elements Container Solution Shiny Facial Skin care Collection Commercial Hygiene Treatment";
+$str = "surf
+surfing surfer
+surfer
+surf wave
+surfboard
+surf board
+california
+vintage beach
+beach sunset
+beach waves
+sea illustration
+vintage sunset
+coast
+sea
+beach
+palm beach
+sunset sea
+tropical paradise
+sea beach
+ocean waves
+tropical beach
+ocean
+paradise
+sea wave
+palm
+beach illustration
+tropical palm
+typography quotes
+sunset
+vintage
+calligraphy
+retro illustration
+tropical illustration
+vintage letter
+sunset illustration
+funky
+70s
+design
+poster elements
+graphic art
+retro poster
+modern illustration
+funky design
+70s retro
+vintage cover
+vintage abstract
+flyer elements
+vintage
+pattern poster
+art design
+graphics
+cover art
+abstract graphics
+trendy
+template
+modern
+wallpaper
+modern design
+modern art
+modern graphic
+elements
+background poster
+retro design
+psychedelic
+graphic background
+old design
+vintage flyer
+retro party
+music art
+beach logo
+personal logo
+logo design
+logo
+logo templates
+flat logo
+business logo
+logo branding
+branding
+branding identity
+flat design
+business branding
+branding design
+business design
+business template
+identity
+branding template
+identity design
+business identity
+template design
+template
+design
+business
+personal
+business flat
+";
 
 $lowerCaseString = strtolower($str);
 
